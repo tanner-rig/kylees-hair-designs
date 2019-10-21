298323c5afbccaebf408e4fa027a91c2afd4b809
Tanner Rigby <tanner.rigby@canopytax.com>
2019-10-21 16:47:50 -0600

    deleting space

--- src/components/root/home.js
+++ src/components/root/home.js
@@ -9,7 +9,7 @@ export default class Home extends Component {
     return (
       <div>
         <h1>Our website is coming soon!</h1> <br />
-        Please visit 
+        Please visit
         <a href="https://www.instagram.com/kylee.hair.designs">
           <img src={InstagramIcon} alt="instagram icon" />
           @kylee.hair.designs
